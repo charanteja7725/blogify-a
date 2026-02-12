1bd10ea122578a4e03e25eff7d8634284a37d9a9
charan7725 <charantejaravuri40@gmail.com>
2026-02-12 11:06:34 +0530

    Add note about modularized post routes

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 // Start server
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running o port ${PORT}`);
 });
 
 // Export app for testing
